130f4b92301fac159ffb7f81677e128e80f0a37d
Rendell Arruda <rendellarruda3@gmail.com>
2022-07-27 19:35:27 -0300

    adiciona atividade-9

---
+++ atividade-9/atividade-9-a.js
@@ -0,0 +1,23 @@
+// Atividade 9
+// Adicione a resposta na linha abaixo de cada exercício
+
+// 1 - Declare uma variável chamada `nome` que receba o seu nome.
+var nome = 'Rendell'
+
+// 2 - Declare uma variável chamada `idade` que receba a sua idade.
+var idade = 30
+
+// 3 - Declare uma variável chamado `usuario` que receba um objeto contendo as propriedades `nome` e `idade`.
+var usuario = { nome: 'Rendell', idade: 30 }
+
+// 4 - Declare uma variável chamada `usuarios` que receba um array contendo 4 números de 2 casas decimais
+var usuarios = [0.11, 0.22, 0.33, 0.44]
+
+// 5 - Calcule o valor do comprimento da circuferência de um círculo com raio 5. A formulá é: C = 2πr
+// Utilize as variáveis abaixo para ajudar a criar a expressão.
+var pi = 3.14;
+var raio = 5;
+
+var circunferenciaCirculo = 2 * pi * raio
+
+console.log(circunferenciaCirculo)
